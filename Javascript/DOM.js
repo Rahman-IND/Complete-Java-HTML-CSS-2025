02e8c1687ea3e676602eb532e6e5c75c749b1783
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-23 19:38:33 +0530

    add JS and CSS changes
     add required JS and CSS elements along with use of DOM

---
+++ Javascript/DOM.js
@@ -0,0 +1,5 @@
+function bodyload() {
+    window.document.images[0].src = "/images/fruits.jpeg";
+    window.document.forms[0].elements[0].placeholder = "Picture Name";
+    window.document.forms[0].elements[1].value = "Submit";
+}
