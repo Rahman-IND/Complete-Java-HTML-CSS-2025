03b8d497130591b20772cb29bc0e4dc2c92b20ce
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-24 12:34:05 +0530

    add JS and CSS changes
    add required JS and CSS elements along with use of DOM

---
+++ Javascript/DOM2.js
@@ -0,0 +1,8 @@
+function bodyload() {
+    // access by name attribute of img element
+    pic.src = "/images/fruits.jpeg"
+    // access by name attribute of form and input field's text element
+    frmName.txtname.placeholder = "Enter Name";
+    // access by name attribute of form and input field's button element
+    frmName.btnRegister.value = "Submit";
+}
