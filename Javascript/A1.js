9100583443f485924be114ae46507b34758cdfa3
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-24 15:14:03 +0530

    add JS and CSS changes
    add required JS and CSS elements along with use of DOM getElementbyTagName method

---
+++ Javascript/A1.js
@@ -0,0 +1,6 @@
+function bodyload() {
+    // getElementsByTagName method
+    // access element through TagName
+    var pics = document.getElementsByTagName("img");
+    alert("Number of Picture in website : " + pics.length);
+}
