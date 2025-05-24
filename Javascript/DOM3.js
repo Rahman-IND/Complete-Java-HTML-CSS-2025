271204f4a22a8474bf5d2c30fff957f6e45e7123
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-24 13:34:08 +0530

    add JS and CSS changes
    add required JS and CSS elements along with use of DOM getElementbyId method

---
+++ Javascript/DOM3.js
@@ -0,0 +1,7 @@
+function bodyload() {
+    // getElementById method
+    // accessing elements using it's id
+    document.getElementById("pic").src = "/images/fruits.jpeg";
+    document.getElementById("txtname").placeholder = "Enter Name";
+    document.getElementById("txtbtn").value = "Submit";
+}
