54658940a962871864ff0bcfdbbc49a0d5877b55
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-24 18:45:31 +0530

    add JS and CSS changes
    add required JS and CSS elements along with use of DOM getElementByClassName method

---
+++ Javascript/A2.js
@@ -0,0 +1,6 @@
+function bodyload() {
+    //access element by class attribute of element
+    // getElementsByClassName method
+    var count = document.getElementsByClassName("bstyle");
+    alert("Number of total 'bstyle' classes on the webpage : " + count.length);
+}
