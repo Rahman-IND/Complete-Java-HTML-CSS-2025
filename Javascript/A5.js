45cb264eb58e819a88349822cdaa6d11c61cdcab
MOTINUR <motinurrahman.dev@gmail.com>
2025-05-26 14:39:27 +0530

    add JS and CSS changes
    add required JS and CSS elements along with use of DOM writeln method

---
+++ Javascript/A5.js
@@ -0,0 +1,5 @@
+function showName() {
+    var uName = document.getElementById("txtname").value;
+    // document.write("Hello! " + uName);  //deprecated
+    document.writeln("Hello! " + uName);
+}
